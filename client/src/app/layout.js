7ee0ce8f6004165de2206bdd80aa6807dee77d42
TarıkSogukpinar <tariksogukpinar@outlook.com>
2023-05-23 11:34:28 +0300

    home section created

--- client/src/app/layout.js
+++ client/src/app/layout.js
@@ -14,7 +14,7 @@ export default function RootLayout({ children }) {
         {" "}
         <Navbar />
         {children}
-        <Footer />
+        {/* <Footer /> */}
       </body>
     </html>
   );
