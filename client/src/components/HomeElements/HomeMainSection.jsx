eabd35fe997766e4cbf86e94c4b2f64b5dced57d
TarıkSogukpinar <tariksogukpinar@outlook.com>
2023-06-08 19:09:31 +0300

    main section edited

--- client/src/components/HomeElements/HomeMainSection.jsx
+++ client/src/components/HomeElements/HomeMainSection.jsx
@@ -63,25 +63,17 @@ export default function HomeMainSection() {
             <div className="mt-8">
               <ul className="space-y-4">
                 <li className="flex items-center gap-4">
-                  <img
-                    src="https://images.unsplash.com/photo-1618354691373-d851c5c3a990?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=830&q=80"
-                    alt=""
-                    className="h-16 w-16 rounded object-cover"
-                  />
-
+               
                   <div>
-                    <h3 className="text-sm text-gray-900">Basic Tee 6-Pack</h3>
+                    <h3 className="text-sm text-gray-900">Example.pdf</h3>
 
                     <dl className="mt-0.5 space-y-px text-[10px] text-gray-600">
                       <div>
-                        <dt className="inline">Size:</dt>
-                        <dd className="inline">XXS</dd>
+                        <dt className="inline">Type: PDF</dt>
+                        <dd className="inline">Size: 5mb</dd>
                       </div>
 
-                      <div>
-                        <dt className="inline">Color:</dt>
-                        <dd className="inline">White</dd>
-                      </div>
+                    
                     </dl>
                   </div>
 
@@ -122,24 +114,14 @@ export default function HomeMainSection() {
                 </li>
 
                 <li className="flex items-center gap-4">
-                  <img
-                    src="https://images.unsplash.com/photo-1618354691373-d851c5c3a990?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=830&q=80"
-                    alt=""
-                    className="h-16 w-16 rounded object-cover"
-                  />
-
+               
                   <div>
-                    <h3 className="text-sm text-gray-900">Basic Tee 6-Pack</h3>
+                    <h3 className="text-sm text-gray-900">Example.pdf</h3>
 
                     <dl className="mt-0.5 space-y-px text-[10px] text-gray-600">
-                      <div>
-                        <dt className="inline">Size:</dt>
-                        <dd className="inline">XXS</dd>
-                      </div>
-
-                      <div>
-                        <dt className="inline">Color:</dt>
-                        <dd className="inline">White</dd>
+                    <div>
+                        <dt className="inline">Type: PDF</dt>
+                        <dd className="inline">Size: 5mb</dd>
                       </div>
                     </dl>
                   </div>
@@ -181,24 +163,15 @@ export default function HomeMainSection() {
                 </li>
 
                 <li className="flex items-center gap-4">
-                  <img
-                    src="https://images.unsplash.com/photo-1618354691373-d851c5c3a990?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=830&q=80"
-                    alt=""
-                    className="h-16 w-16 rounded object-cover"
-                  />
+                 
 
                   <div>
-                    <h3 className="text-sm text-gray-900">Basic Tee 6-Pack</h3>
+                    <h3 className="text-sm text-gray-900">Example.pdf</h3>
 
                     <dl className="mt-0.5 space-y-px text-[10px] text-gray-600">
-                      <div>
-                        <dt className="inline">Size:</dt>
-                        <dd className="inline">XXS</dd>
-                      </div>
-
-                      <div>
-                        <dt className="inline">Color:</dt>
-                        <dd className="inline">White</dd>
+                    <div>
+                        <dt className="inline">Type: PDF</dt>
+                        <dd className="inline">Size: 5mb</dd>
                       </div>
                     </dl>
                   </div>
